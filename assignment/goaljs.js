35f3d9fd0e3527d0fdad206df6b95afce2c1d0f0
Leanne Tan <leannetan97@hotmail.com>
2018-04-08 22:59:21 +0800

    Added favicon for each page & using js calculate Goal%

---
+++ assignment/goaljs.js
@@ -0,0 +1,12 @@
+
+window.addEventListener("load",function(){
+	var targetGoal = document.getElementById("TargetGoal").innerHTML;
+	var moneySave = document.getElementById("MoneySaved").innerHTML;
+	var moneyRequired = document.getElementById("MoneyRequired").innerHTML;
+	console.log("test goaljs");
+	console.log(document.getElementById("TargetGoal").innerHTML);
+	// console.log(moneyRequired);
+	// console.log(moneySave);
+	document.getElementById("mrPercent").innerHTML = ((parseInt(moneyRequired)/parseInt(targetGoal))*100).toFixed(0) + "%";
+	document.getElementById("msPercent").innerHTML = ((parseInt(moneySave)/parseInt(targetGoal))*100).toFixed(0) + "%";
+});
